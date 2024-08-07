58f44a4c367bbb4383dc4575685714d0a107040b
Volodymyr Shyrmanov <volodymyr.shyrmanov@nxis.com>
2024-08-07 14:31:46 +0200

    di example

---
+++ ResilientApi/ExternalClient.cs
@@ -0,0 +1,25 @@
+﻿namespace ResilientApi;
+
+public interface IExternalClient
+{
+    Task<string> GetDataFromApi(CancellationToken token);
+}
+
+public class ExternalClient : IExternalClient
+{
+    private readonly ILogger<ExternalClient> _logger;
+
+    public ExternalClient(ILogger<ExternalClient> logger)
+    {
+        _logger = logger;
+    }
+    
+    public async Task<string> GetDataFromApi(CancellationToken token)
+    {
+        await Task.Delay(10_000, token);
+
+        _logger.LogInformation("Got the result");
+        
+        return "Finally results";
+    }
+}
---
+++ ResilientApi/Program.cs
@@ -0,0 +1,55 @@
+using Polly;
+using Polly.Fallback;
+using Polly.Registry;
+using Polly.Retry;
+using Polly.Timeout;
+using ResilientApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IExternalClient, ExternalClient>();
+
+builder.Services.AddResiliencePipeline("retry", pipelineBuilder =>
+    pipelineBuilder.AddRetry(new RetryStrategyOptions
+    {
+        Delay = TimeSpan.FromSeconds(1),
+        MaxRetryAttempts = 2,
+        BackoffType = DelayBackoffType.Exponential,
+        UseJitter = true
+    }));
+
+builder.Services.AddResiliencePipeline<string, string>("fallback", pipelineBuilder =>
+    pipelineBuilder.AddFallback(new FallbackStrategyOptions<string>
+    {
+        FallbackAction = _ => Outcome.FromResultAsValueTask("Fallback value")
+    }).AddTimeout(new TimeoutStrategyOptions
+    {
+        Timeout = TimeSpan.FromSeconds(1)
+    }));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/test", async (CancellationToken cancellationToken, IExternalClient httpClient, ResiliencePipelineProvider<string> pipelineProvider) =>
+    {
+        ResiliencePipeline<string> pipeline = pipelineProvider.GetPipeline<string>("fallback");
+        var result = await pipeline.ExecuteAsync(async token => await httpClient.GetDataFromApi(cancellationToken), cancellationToken);
+        return result;
+    })
+    .WithName("test")
+    .WithOpenApi();
+
+app.Run();
